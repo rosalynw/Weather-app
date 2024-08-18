c69f55afdde13efb680ce42b1872dbdf988f2bb4
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2024-08-18 10:48:25 -0400

    style: Add media queries for phones

--- script.js
+++ script.js
@@ -23,3 +23,102 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+const app = document.querySelector('.weather-app');
+const temp = document.querySelector('.temp');
+const dateOuput = document.querySelector('.date');
+const timeOutput = document.querySelector('.time');
+const conditionOutput = document.querySelector('.condition');
+const nameOutput = document.querySelector('.name');
+const icon = document.querySelector('.icon');
+const cloudOutput = document.querySelector('.clout');
+const humidityOutput = document.querySelector('.humidity');
+const windOutput = document.querySelector('.wind');
+const form = document.querySelector('locationInput');
+const search = document.querySelector('.search');
+const cities = document.querySelectorAll('.city');
+
+let cityInput = "London";
+
+cities.forEach((city) => {
+    city.addEventListener('click', (e) => {
+        cityInput = e.target.innerHTML;
+
+        fetchWeatherData();
+        app.style.opacity = "0";
+    })
+})
+
+form.addEventListener('submit', (e) => {
+    if(search.value.length == 0) {
+        alert('Please type in a city name');
+    } else {
+        cityInput = search.value;
+
+        fetchWeatherData();
+        search.value = "";
+        app.style.opacity = "0";
+    }
+
+    e.preventDefault();
+})
+
+function dayOfTheWeek(day, month, year) {
+    const weekday = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    return weekday[new Date(`${day}/${month}/${year}`).getDay()]
+};
+
+function fetchWeatherData() {
+
+    fetch(`https://www.weatherapi.com/v1/current.json?key=ec9c9427be5442c1b0602352241808=${cityInput}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        temp.innerHTML = data.current.temp_c + "&#176;";
+        conditionOutput.innerHTML = data.current.condition.text;
+
+        const date = data.location.localtime;
+        const y = parseInt(date.substr(0,4));
+        const m = parseInt(date.substr(5,2));
+        const d = parseInt(date.substr(8,2));
+        const itme = date.substr(11);
+
+        dateOuput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d}, ${m} ${y}`;
+        timeOutput.innerHTML = time;
+        
+        nameOutput.innerHTML = data.location.name;
+
+        const iconId = data.current.condition.icon.substr(
+            "//cdn.weatherapi.com/weather/64x64/".length
+        );
+
+        icon.src = "./icon" + iconId;
+
+        cloudOutput.innerHTML = data.current.cloud + "%";
+        humidityOutput.innerHTML = data.current.humidity + "%";
+        windOutput.innerHTML = data.current.humidity + "km/h";
+
+        let timeOfDay = "day";
+
+        const code = data.current.condition.conde;
+
+        if(!data.current.is_day) {
+            timeOfDay = "night";
+        }
+
+        if(code == 1000) {
+            app.style.backgroundImage = `url(./images/${timeOfDay}/stars.jpg)`;
+
+            btn.style.background = "#e5ba92";
+        }
+    })
+}
